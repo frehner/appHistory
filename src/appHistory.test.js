3bb4812f4ffa96964ef72b4a5066ed100c0f3923
Anthony Frehner <afrehner.work@gmail.com>
2021-02-12 16:15:39 -0700

    working on an implementation and tests

--- src/appHistory.test.js
+++ src/appHistory.test.js
@@ -1,5 +1,82 @@
-describe("appHistory", () => {
-  it("should work", () => {
-    expect(true).toBe(true);
+import { AppHistory } from "./appHistory";
+
+describe("updateCurrentEntry", () => {
+  it("only url: updates url but not the state", () => {
+    const appHistory = new AppHistory();
+    appHistory.updateCurrentEntry({ state: "test" });
+
+    const { url: oldUrl, state: oldState } = appHistory.currentEntry;
+
+    const updatedUrl = "/newUrl";
+    appHistory.updateCurrentEntry({ url: updatedUrl });
+
+    const { url: newUrl, state: newState } = appHistory.currentEntry;
+
+    expect(oldUrl).not.toEqual(newUrl);
+    expect(oldState).toEqual(newState);
+    expect(newUrl).toEqual(updatedUrl);
+  });
+
+  it("only state: updates state but not the url", () => {
+    const appHistory = new AppHistory();
+
+    const { url: oldUrl, state: oldState } = appHistory.currentEntry;
+
+    const updatedState = "newState";
+    appHistory.updateCurrentEntry({ state: updatedState });
+
+    const { url: newUrl, state: newState } = appHistory.currentEntry;
+
+    expect(oldUrl).toEqual(newUrl);
+    expect(oldState).not.toEqual(newState);
+    expect(newState).toEqual(updatedState);
+  });
+
+  it("can null out the state", () => {
+    const appHistory = new AppHistory();
+    appHistory.updateCurrentEntry({ state: "before" });
+
+    const { state: oldState } = appHistory.currentEntry;
+
+    const updatedState = null;
+    appHistory.updateCurrentEntry({ state: updatedState });
+
+    const { state: newState } = appHistory.currentEntry;
+
+    expect(oldState).not.toEqual(newState);
+    expect(newState).toEqual(updatedState);
+  });
+
+  it("can update both state and url at the same time", () => {
+    const appHistory = new AppHistory();
+
+    const { url: oldUrl, state: oldState } = appHistory.currentEntry;
+
+    const updatedState = "newState";
+    const updatedUrl = "/newUrl";
+    appHistory.updateCurrentEntry({ state: updatedState, url: updatedUrl });
+
+    const { url: newUrl, state: newState } = appHistory.currentEntry;
+
+    expect(oldUrl).not.toEqual(newUrl);
+    expect(oldState).not.toEqual(newState);
+
+    expect(newUrl).toEqual(updatedUrl);
+    expect(newState).toEqual(updatedState);
+  });
+
+  it("does not add a new entry to the entries list", () => {
+    const appHistory = new AppHistory();
+
+    const oldEntries = appHistory.entries;
+
+    const newState = "newState";
+    appHistory.updateCurrentEntry({ state: newState });
+
+    const newEntries = appHistory.entries;
+
+    expect(oldEntries).not.toEqual(newEntries);
+    expect(newEntries.length).toEqual(1);
+    expect(newEntries[0].state).toEqual(newState);
   });
 });
--- src/appHistory.ts
+++ src/appHistory.ts
@@ -1,31 +1,43 @@
 import { fakeRandomId } from "./helpers";
 
-export const appHistory: AppHistory = {
-  currentEntry: Object.freeze({
-    key: fakeRandomId(),
-    url: "http",
-    state: null,
-    sameDocument: true,
-    onnavigateto: () => {},
-    onupcomingnavigate: () => {},
-    oncurrententrychange: () => {},
-  }),
-  updateCurrentEntry,
-  pushNewEntry: (options: AppHistoryEntryOptions) => Promise.resolve(undefined),
-  entries: Object.freeze([]),
-  navigateTo: () => Promise.resolve(undefined),
-  back: () => Promise.resolve(undefined),
-  forward: () => Promise.resolve(undefined),
-};
+export class AppHistory {
+  currentEntry: Readonly<AppHistoryEntry>;
+  entries: Readonly<AppHistoryEntry[]>;
 
-function updateCurrentEntry(options: AppHistoryEntryOptions): void {
-  const newState = options?.state !== null ? history.state : options.state;
-  const newUrl = options?.url ?? location.href;
+  updateCurrentEntry(options: AppHistoryEntryOptions): void {
+    const newState =
+      options?.state !== undefined ? options.state : this.currentEntry.state;
+    const newUrl = options?.url ?? this.currentEntry.url;
+    const newEntry = this.createNewEntry({ newUrl, newState });
+    this.currentEntry = newEntry;
+    const [replacedEntry, ...restEntries] = this.entries;
+    this.entries = Object.freeze([newEntry, ...restEntries]);
+  }
+
+  private createNewEntry({
+    newUrl,
+    newState,
+  }: {
+    newUrl?: string;
+    newState?: any | null;
+  }): Readonly<AppHistoryEntry> {
+    return Object.freeze({
+      key: fakeRandomId(),
+      url: newUrl,
+      state: newState,
+      sameDocument: true,
+      onnavigateto: () => {},
+      onupcomingnavigate: () => {},
+      oncurrententrychange: () => {},
+    });
+  }
+
+  constructor() {
+    this.currentEntry = this.createNewEntry({ newUrl: "test" });
+    this.entries = Object.freeze([this.currentEntry]);
+  }
 }
 
-export type AppHistoryEntryKey = string;
-
-// interface AppHistoryEntry extends EventTarget {
 interface AppHistoryEntry {
   key: AppHistoryEntryKey;
   url: string;
@@ -36,18 +48,9 @@ interface AppHistoryEntry {
   oncurrententrychange: Readonly<EventHandlerNonNull>;
 }
 
+export type AppHistoryEntryKey = string;
+
 type AppHistoryEntryOptions = {
   url?: string;
   state?: any | null;
 };
-
-// interface AppHistory extends EventTarget {
-interface AppHistory {
-  currentEntry: Readonly<AppHistoryEntry>;
-  updateCurrentEntry: (options?: AppHistoryEntryOptions) => void;
-  pushNewEntry: (options?: AppHistoryEntryOptions) => Promise<undefined>;
-  entries: readonly AppHistoryEntry[];
-  navigateTo: (key: AppHistoryEntryKey) => Promise<undefined>;
-  back: () => Promise<undefined>;
-  forward: () => Promise<undefined>;
-}
